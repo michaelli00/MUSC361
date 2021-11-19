96212c9fc807bbae5e8a190fc634db63de48da1a
Michael Li <mli25782@umd.edu>
2021-11-19 15:16:19 -0500

    11/19/21

--- big-project/src/utils/Constants.js
+++
@@ -1,8 +0,0 @@
-export const TWELVE_TONE_DROPDOWN_OPTIONS = [...Array(12).keys()].map(val => {
-  return {
-    value: parseInt(val),
-    label: parseInt(val),
-  };
-});
-
-export const TWELVE_TONE_TO_NOTE_MAPPING = ['c', '_d', 'd', '_e', 'e', 'f', '^f', 'g', '_a', 'a', '_b', 'b']
